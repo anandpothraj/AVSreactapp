c709e2303d534d757e724f305dd0b6bfeae06f03
Anand <anandpothraj11052001@gmail.com>
2021-06-01 16:35:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState, useEffect} from 'react' ;
+import Movie from './components/Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1da65723b2764bbb257611a20829c74f&page=1";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=1da65723b2764bbb257611a20829c74f&query=";
+
+function App (){
+    const [ movies , setMovies ] = useState([]);
+    const [ searchTerm , setSearchTerm ] = useState("");
+
+    useEffect(() => {
+        getMovies(FEATURED_API);
+    },[]);
+
+    const getMovies = (API) => {
+        fetch(API)
+            .then((res) => res.json())
+            .then((data) => {
+                setMovies(data.results);
+            });
+    };
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+
+        if(searchTerm) {
+            getMovies(SEARCH_API + searchTerm);
+
+            setSearchTerm("");
+        }
+    };
+
+    const handleOnChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const url = "";
+
+
+    return(
+        <>
+        <header>
+            <h3 className="title"><a href={url}>AVS</a></h3>
+            <form onSubmit={handleOnSubmit}>
+                <input type="search" placeholder="Search..." value={searchTerm} className="search" onChange={handleOnChange}/>
+            </form>
+        </header>
+        <div className="movie-container">
+            {movies.length > 0 &&  
+                movies.map((movie) => <Movie key=
+                {movie.id} {...movie}/> )}
+        </div>
+        </>
+    );
+};
 
 export default App;
